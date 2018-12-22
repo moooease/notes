624aed28c3140f776bcd961103f820e83236b0df
moooease <mueeza.96@gmail.com>
2018-12-22 04:30:01 -0600

    time now refreshes every second

--- script.js
+++ script.js
@@ -10,14 +10,18 @@ let save = () => {
 
 editorContent.onkeyup = () => save()
 
-document.getElementById("para1").innerHTML = formatAMPM();
+
 
 function formatAMPM() {
-var d = new Date(),
+  var str = "";
+  var d = new Date(),
     minutes = d.getMinutes().toString().length == 1 ? '0'+d.getMinutes() : d.getMinutes(),
     hours = d.getHours().toString().length == 1 ? '0'+d.getHours() : d.getHours(),
     ampm = d.getHours() >= 12 ? 'pm' : 'am',
     months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'],
     days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-return'Its '+hours+':'+minutes+ampm +' on a '+ days[d.getDay()]+'.';
+str += 'Its '+hours+':'+minutes+ampm +' on a '+ days[d.getDay()]+'.';
+document.getElementById("date").innerHTML = str;
 }
+
+setInterval(formatAMPM, 1000);

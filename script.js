e3e18090c1a6c68041d8f0be2928f4e658e30c1c
moooease <moooease@gmail.com>
2018-12-22 03:18:57 -0600

    intial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+
+
+const editorContent = document.querySelector('.content')
+editorContent.innerHTML = localStorage['content']
+
+let save = () => {
+  localStorage['content'] = editorContent.innerHTML
+}
+
+editorContent.onkeyup = () => save()
+
+document.getElementById("para1").innerHTML = formatAMPM();
+
+function formatAMPM() {
+var d = new Date(),
+    minutes = d.getMinutes().toString().length == 1 ? '0'+d.getMinutes() : d.getMinutes(),
+    hours = d.getHours().toString().length == 1 ? '0'+d.getHours() : d.getHours(),
+    ampm = d.getHours() >= 12 ? 'pm' : 'am',
+    months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'],
+    days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+return'Its '+hours+':'+minutes+ampm +' on a '+ days[d.getDay()]+'.';
+}

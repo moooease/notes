ec07acb296e63796604f79817e0a5f5a15f9231e
moooease <mueeza.96@gmail.com>
2018-12-22 12:39:23 -0600

    fixed undefined issue and made input selected by default

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 
-
+const editorTitle = document.querySelector('.title')
 const editorContent = document.querySelector('.content')
-editorContent.innerHTML = localStorage['content']
-const defaultContent = '  '
+const defaultTitle = 'Whats on your mind?'
+const defaultContent = ''
+
+editorTitle.innerHTML = localStorage['title'] || defaultTitle
+editorContent.innerHTML = localStorage['content'] || defaultContent
 
 let save = () => {
-  localStorage['content'] = editorContent.innerHTML || defaultContent
+  localStorage['title'] = editorTitle.innerHTML
+  localStorage['content'] = editorContent.innerHTML
 }
 
+editorTitle.onkeyup = () => save()
 editorContent.onkeyup = () => save()
-
 
 
 function formatAMPM() {
@@ -18,10 +22,13 @@ function formatAMPM() {
     minutes = d.getMinutes().toString().length == 1 ? '0'+d.getMinutes() : d.getMinutes(),
     hours = d.getHours().toString().length == 1 ? '0'+d.getHours() : d.getHours(),
     ampm = d.getHours() >= 12 ? 'pm' : 'am',
-    months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'],
     days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
 str += 'Its '+hours+':'+minutes+ampm +' on a '+ days[d.getDay()]+'.';
 document.getElementById("date").innerHTML = str;
 }
 
 setInterval(formatAMPM, 1000);
+
+var input = document.getElementById('myTextInput');
+input.focus();
+input.select();
